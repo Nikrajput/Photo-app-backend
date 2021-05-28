c7b0cfaf03366de3b45f9ef7df7e1953516cbd43
NikRajput <nikrajput8383@gmail.com>
2021-05-28 21:50:58 +0530

    uplaoding image

--- api/Image.js
+++ api/Image.js
@@ -3,10 +3,13 @@ const router=express.Router()
 const upload=require('../config/multer')
 const cloudinary=require('../config/cloudinary')
 const Image=require('../models/Image')
+const sharp=require('sharp')
 
 router.post('/upload',upload.single("image"),async(req,res)=>{
     
     try{
+        const thumbnailImage=await sharp(req.file.path).resize(200,200)
+        console.log(thumbnailImage)
         const result=await cloudinary.uploader.upload(req.file.path)
         const username=req.file.originalname.substr(0,req.file.originalname.length-4)
         const image=new Image({

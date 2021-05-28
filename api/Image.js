b32b4cf1f6aee7f54125faf6039a654b7fb908ca
NikRajput <nikrajput8383@gmail.com>
2021-05-28 15:30:40 +0530

    uplaoding image

--- api/Image.js
+++ api/Image.js
@@ -3,7 +3,7 @@ const router=express.Router()
 const upload=require('../config/multer.js')
 
 router.post('/upload',upload.single("image"),async(req,res)=>{
-    console.log(req.body)
+    console.log(req.body.userDetails)
     console.log(req.file)
     res.send("send")
 })

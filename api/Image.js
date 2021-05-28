fb9684dbaaa17dd7cf94ebfb1f3d16b6b9a872b9
NikRajput <nikrajput8383@gmail.com>
2021-05-28 12:56:18 +0530

    uplaoding image

---
+++ api/Image.js
@@ -0,0 +1,11 @@
+const express=require('express')
+const router=express.Router()
+const upload=require('../config/multer.js')
+
+router.post('/upload',upload.single("image"),async(req,res)=>{
+    console.log(req.file)
+    res.send("send")
+})
+
+
+module.exports=router
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express=require('express')
 const app=express()
 
 const userRouter=require('./api/User')
+const imageRouter=require('./api/Image')
 
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
@@ -11,5 +12,6 @@ const connectDb=require('./config/db.js')
 connectDb()
 
 app.use('/user',userRouter)
+app.use('/image',imageRouter)
 
 app.listen(process.env.PORT || 3123)

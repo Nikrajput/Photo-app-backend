2cc2f26d5e5b0eea078e3033ff309982df15bd8a
NikRajput <nikrajput8383@gmail.com>
2021-05-29 10:02:18 +0530

    counting faces

--- api/Image.js
+++ api/Image.js
@@ -4,14 +4,27 @@ const upload=require('../config/multer')
 const cloudinary=require('../config/cloudinary')
 const Image=require('../models/Image')
 const sharp=require('sharp')
+const axios=require('axios').default
 
 router.post('/upload',upload.single("image"),async(req,res)=>{
     
     try{
-        const thumbnailImage=await sharp(req.file.path).resize(200,200).toFile('uploads/' + 'thumbnails-' + req.file.originalname)
-        console.log(thumbnailImage)
+        await sharp(req.file.path).resize(200,200).toFile('uploads/' + 'thumbnails-' + req.file.originalname)
         const result=await cloudinary.uploader.upload('uploads/' + 'thumbnails-' + req.file.originalname)
         const username=req.file.originalname.substr(0,req.file.originalname.length-4)
+
+        var options = {
+            method: 'POST',
+            url: 'https://face-detection6.p.rapidapi.com/img/face-age-gender',
+            headers: {
+                'content-type': 'application/json',
+                'x-rapidapi-key': 'de27c4c4c7msh9366d5082f1d78ep1ca312jsn910995513853',
+                'x-rapidapi-host': 'face-detection6.p.rapidapi.com'
+            },
+            data: {url: `${result.secure_url}`, accuracy_boost: 3}
+        }
+        const response=await axios.request(options)
+        console.log(response)
         const image=new Image({
             username:username,
             countOfPeople:0,
